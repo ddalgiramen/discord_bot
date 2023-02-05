969846e33b4ce51fb58c0285791e2651e5dc4d41
potyama <kamikaze117117@gmail.com>
2023-02-06 07:10:45 +0900

    send :ohajett: message

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,8 @@
 import discord
 import os
+from discord.ext import tasks
 from dotenv import load_dotenv
-
+from datetime import datetime
 
 load_dotenv()
 intents = discord.Intents.default()
@@ -10,8 +11,8 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-
+    print(f'We have logged in as {client.user}\n')
+    loop.start()
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -20,6 +21,15 @@ async def on_message(message):
         await message.channel.send('Hello!')
     if message.content.startswith('<:ohajett:1059962614869938278>'):
         await message.add_reaction("<:ohajett:1059962614869938278>")
+        print(message.author.id)
 
+
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '07:30':
+        channel = client.get_channel(1010108583670722590)
+        await channel.send('<:ohajett:1059962614869938278>')
 
 client.run(os.environ['TOKEN'])
